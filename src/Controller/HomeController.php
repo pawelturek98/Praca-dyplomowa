08a4c4a2e8a442e21eab9d3926dbed1dd3bd4e2c
PawiX8 <pawelturek98@gmail.com>
2022-10-24 15:48:56 +0200

    [PI-1] Fixed security

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,14 +7,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app.home')]
     public function index(): Response
     {
-        return $this->render('home.html.twig', [
-        ]);
+        return new Response();
     }
 }
 
